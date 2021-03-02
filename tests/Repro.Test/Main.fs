7141634188693750331144eaa7e98f12b6bc2363
Ryan Riley <ryanriley@live.com>
2021-03-02 15:38:02 -0600

    Initial commit

---
+++ tests/Repro.Test/Main.fs
@@ -0,0 +1,6 @@
+﻿module Repro.Test
+open Expecto
+
+[<EntryPoint>]
+let main argv =
+    Tests.runTestsInAssembly defaultConfig argv
---
+++ tests/Repro.Test/Sample.fs
@@ -0,0 +1,24 @@
+module Tests
+
+open Expecto
+
+[<Tests>]
+let tests =
+    testList
+        "samples"
+        [ testCase "universe exists (╭ರᴥ•́)"
+          <| fun _ ->
+              let subject = true
+              Expect.isTrue subject "I compute, therefore I am."
+
+          testCase "I'm skipped (should skip)"
+          <| fun _ -> Tests.skiptest "Yup, waiting for a sunny day..."
+
+          testCase "contains things"
+          <| fun _ -> Expect.containsAll [| 2; 3; 4 |] [| 2; 4 |] "This is the case; {2,3,4} contains {2,4}"
+
+          test "from whom" {
+              let expected = "from F#"
+              let actual = Repro.from "F#"
+              "╰〳 ಠ 益 ಠೃ 〵╯" |> Expect.equal actual expected
+          } ]
